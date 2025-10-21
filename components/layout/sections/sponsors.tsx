3170c02f7cf510a3ecee072a7c92bb91bd18789e
Nutvs <kumtupvs@gmail.com>
2025-10-21 11:54:45 +0700

    Update sponsors.tsx
    Change Marquee Line one two three

--- components/layout/sections/sponsors.tsx
+++ components/layout/sections/sponsors.tsx
@@ -1,137 +1,72 @@
 "use client";
 
-import { Icon } from "@/components/ui/icon";
 import { Marquee } from "@devnomic/marquee";
 import "@devnomic/marquee/dist/index.css";
-import { string } from "zod";
-interface sponsorsProps {
+
+interface Sponsor {
   name: string;
 }
 
-const sponsors: sponsorsProps[] = [
-  {
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
-  {
-
-    name: "POWERFUL THAILAND",
-  },
+const sponsors: Sponsor[] = [
+  { name: "POWERFUL THAILAND" },
+  { name: "POWERFUL THAILAND" },
+  { name: "POWERFUL THAILAND" },
+  { name: "POWERFUL THAILAND" },
+  { name: "POWERFUL THAILAND" },
+  { name: "POWERFUL THAILAND" },
 ];
 
-interface sponsorsPropstwo {
-  name: string;
-}
+const sponsorsTwo: Sponsor[] = [
+  { name: "THAILANDING" },
+  { name: "THAILANDING" },
+  { name: "THAILANDING" },
+  { name: "THAILANDING" },
+  { name: "THAILANDING" },
+];
 
-const sponsorsTwo: sponsorsPropstwo[] = [
-  {
-    name: "THAILANDING",
-  },
-  {
-
-    name: "THAILANDING",
-  },
-  {
-
-    name: "THAILANDING",
-  },
-  {
-
-    name: "THAILANDING",
-  },
-  {
-    name: "THAILANDING",
-  },
-  {
-    name: "THAILANDING",
-  },
-  {
-    name: "THAILANDING",
-  },
+const sponsorsThree: Sponsor[] = [
+  { name: "THAILAND OUTSTANDING" },
+  { name: "THAILAND OUTSTANDING" },
+  { name: "THAILAND OUTSTANDING" },
+  { name: "THAILAND OUTSTANDING" },
+  { name: "THAILAND OUTSTANDING" },
 ];
 
 export const SponsorsSection = () => {
   return (
-    <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32">
-
-      {/* Marquee 1st */}
-
+    <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32 space-y-8">
+      
+      {/* Marquee 1 */}
       <div className="mx-auto">
-        <Marquee
-          className="gap-[3rem]"
-          fade
-          innerClassName="gap-[3rem]"
-          pauseOnHover
-        >
-          {sponsors.map(({ name }) => (
-            <div
-              key={name}
-              className="flex items-center text-xl md:text-5xl font-medium"
-            >
+        <Marquee className="gap-[3rem]" fade innerClassName="gap-[3rem]" pauseOnHover>
+          {sponsors.map(({ name }, i) => (
+            <div key={i} className="flex items-center text-xl md:text-5xl font-medium">
               {name}
             </div>
           ))}
         </Marquee>
       </div>
 
-      {/* Marquee 2nd */}
-
+      {/* Marquee 2 */}
       <div className="mx-auto">
-        <Marquee
-          className="gap-[3rem]"
-          fade
-          innerClassName="gap-[3rem]"
-          pauseOnHover
-        >
-          {sponsors.map(({ name }) => (
-            <div
-              key={name}
-              className="flex items-center text-xl md:text-5xl font-medium"
-            >
+        <Marquee className="gap-[3rem]" fade innerClassName="gap-[3rem]" pauseOnHover>
+          {sponsorsTwo.map(({ name }, i) => (
+            <div key={i} className="flex items-center text-xl md:text-5xl font-medium">
               {name}
             </div>
           ))}
         </Marquee>
       </div>
 
-      {/* Marquee 3rd */}
-
+      {/* Marquee 3 */}
       <div className="mx-auto">
-        <Marquee
-          className="gap-[3rem]"
-          fade
-          innerClassName="gap-[3rem]"
-          pauseOnHover
-        >
-          {sponsors.map(({ name }) => (
-            <div
-              key={name}
-              className="flex items-center text-xl md:text-5xl font-medium"
-            >
+        <Marquee className="gap-[3rem]" fade innerClassName="gap-[3rem]" pauseOnHover>
+          {sponsorsThree.map(({ name }, i) => (
+            <div key={i} className="flex items-center text-xl md:text-5xl font-medium">
               {name}
             </div>
           ))}
         </Marquee>
-
       </div>
     </section>
   );
