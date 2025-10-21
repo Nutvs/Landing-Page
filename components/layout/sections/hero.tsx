0afabcffda355d61e3c0f216d49acbcb1880c7f8
Nutvs <kumtupvs@gmail.com>
2025-10-21 15:21:13 +0700

    fixed space
    space 60

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -10,8 +10,8 @@ export const HeroSection = () => {
   const { theme } = useTheme();
   return (
     <section className="container w-full">
-      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
-        <div className="text-center space-y-8">
+      <div className="grid place-items-center space-y-60 lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+        <div className="text-center space-y-12">
           <div className="max-w-screen-md mx-auto text-center text-3xl md:text-5xl font-bold">
             <h1>
               <span className="text-transparent px-1 bg-gradient-to-r from-[#0E2B59] to-primary bg-clip-text">
@@ -26,22 +26,12 @@ export const HeroSection = () => {
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
-              Get Started
-              <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
-            </Button>
 
             <Button
               asChild
               variant="secondary"
               className="w-5/6 md:w-1/4 font-bold"
             >
-              <Link
-                href="https://github.com/nobruf/shadcn-landing-page.git"
-                target="_blank"
-              >
-                Github respository
-              </Link>
             </Button>
           </div>
         </div>
